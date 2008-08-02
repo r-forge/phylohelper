9e3dea5cbc98ff3e3d4c6835ca84c7f5f4b925f1
mbutler (Marguerite Butler) <mbutler@hawaii.edu>
2008-08-02 17:02:16 +0000

    edgeL now works on node name
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phylohelper@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/edgeL.R
+++ pkg/R/edgeL.R
@@ -1,14 +1,21 @@
 ### Edge length accessor to grab by node number
 ### examples:  edgeL( tree4, 9)
 ###   edgeL(tree4, c(7,8,9))
-###   MRCA(tree4,"Homo", "Pongo")
+###   edgeL(tree4, MRCA(tree4,"Homo", "Pongo"))
+###   edgeL(tree4, "Homo")
+###   edgeL(tree4, c("Homo", "Galago"))
 
 edgeL <- function(tree, nx) {
-	node <- tree@edge[,2]
-	return(tree@edge.length[node %in% nx])
+	if (is.numeric(nx)) {
+		node <- tree@edge[,2]
+		return(tree@edge.length[node %in% nx])
+	} else {
+		nodenames <- c(tree@node.label[-1], tree@tip.label)
+		return(tree@edge.length[nodenames %in% nx])
 	}
+}
 
-## Tests if the given node number is the root
+## Tests if the given node number or name is the root 
 
 is.root <- function(tree, x) {
 	if ( length(ancestor(tree, x))>0 ) return(FALSE)
