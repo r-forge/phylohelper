9cda730fd330ba6fc2d72e46b56e7cfd7b9539ba
zakalisz (Ada Kaliszewska) <zakalisz@fas.harvard.edu>
2008-08-03 18:25:30 +0000

    revised confile.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phylohelper@22 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/contree.R
+++ pkg/R/contree.R
@@ -1,10 +1,11 @@
 #plots contree with brlens and pps for clades
-#file="carnivore_molecular.nex.con"
 
 contree<-function(confile=confile,brlens=F,savepdf=F){
 	library(ape)
 	read.nexus(file=confile)->x
-	pptree<-x[[1]]
+	file.name<-attr(x,"origin")
+	pptree1<-x[[1]]
+	pptree <- ladderize(pptree1, right=FALSE)
 	plot(pptree)
 	if (brlens==T) {
 		plot(pptree)
@@ -16,13 +17,13 @@ contree<-function(confile=confile,brlens=F,savepdf=F){
 		adj=c(1.2,-0.5),frame="n",cex=0.8,font=2)}
 	if (savepdf==T) {
 		if (brlens==T) {
-			pdf(file="contree%02d.pdf")
+			pdf(file=paste(file.name,".pdf",sep=""))
 			plot(pptree)
 			nodelabels(pptree$node.label, 
 			adj=c(1.2,-0.5),frame="n",cex=0.8,font=2)
 			dev.off()}
 		else if (brlens==F) {
-			pdf(file="contree%02d.pdf")
+			pdf(file=paste(file.name,".pdf",sep=""))
 			plot(pptree,use.edge.length=F)
 			nodelabels(pptree$node.label, 
 			adj=c(1.2,-0.5),frame="n",cex=0.8,font=2)
@@ -31,5 +32,3 @@ contree<-function(confile=confile,brlens=F,savepdf=F){
 	}
 
 	
-	#if brlens=F, will give ultrametric tree with grafen branch lengths
-	#tell folks what name pdf gets and where it goes -- same folder
